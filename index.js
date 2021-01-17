2055f8f01c54bee607b500e58eaaa64ee02f78c2
Jake Ols <jake@kurtnoble.com>
2021-01-16 19:44:11 -0800

    update to work on directories

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const fs = require("fs").promises;
+const fsSynchronus = require("fs"); // sync functions won't work with promisify
 const HTMLParser = require("node-html-parser");
-const config = require('./config.json');
+const config = require("./config.json");
+const path = require("path");
+
+const getAllFiles = function (dir, fileArr) {
+  let files = fsSynchronus.readdirSync(dir);
+  fileArr = fileArr || [];
+  files.forEach(function (file) {
+    if (fsSynchronus.statSync(dir + "/" + file).isDirectory()) {
+      fileArr = getAllFiles(dir + "/" + file, fileArr);
+    } else {
+      fileArr.push(path.join(__dirname, dir, "/", file));
+    }
+  });
+  return fileArr;
+};
 
 async function main() {
   let css = await fs.readFile(config.cssFilePath, "utf8");
@@ -8,26 +23,35 @@ async function main() {
     css.match(/(?:[\.]{1})([a-zA-Z_]+[\w-_]*)(?:[\s\.\,\{\>#\:]{0})/gim)
   );
 
-  let html = await fs.readFile(config.htmlFilePath, "utf8");
+  // check if set config is directory, or file
+  let isDirectory = fsSynchronus.statSync(config.htmlFilePath).isDirectory();
+  if (!isDirectory) {
+    let html = await fs.readFile(config.htmlFilePath, "utf8");
+    await removeUsedClasses(selectors, html);
+  } else if (isDirectory) {
+    const files = getAllFiles(config.htmlFilePath);
+    for (const file of files) {
+      let html = await fs.readFile(file, "utf8");
+      await removeUsedClasses(selectors, html);
+    }
+  } else {
+    console.log("Error reading from file");
+  }
 
-  const unusedSelectors = await removeUsedClasses(selectors, html);
-  console.log(unusedSelectors);
+  console.log("Unused selectors are:", [...selectors].join(" "));
 }
 
 /**
- * Removes used selectors from set, returning only unused css selectors
+ * Removes used selectors from set (mutating it)
  */
 async function removeUsedClasses(css, html) {
   const root = HTMLParser.parse(html);
-
   css.forEach((item) => {
     let selected = root.querySelectorAll(item);
     if (selected.length > 0) {
       css.delete(item);
     }
   });
-
-  return css;
 }
 
 main();
